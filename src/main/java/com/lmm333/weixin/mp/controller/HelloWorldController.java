297e65ba7687780819bcd0a14e1e3d071931d791
lmmsoft <lmm333@126.com>
2018-08-10 13:45:55 +0800

    Add CorsConfiguration for debug local ajax requests

--- src/main/java/com/lmm333/weixin/mp/controller/HelloWorldController.java
+++ src/main/java/com/lmm333/weixin/mp/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.lmm333.weixin.mp.controller;
 import com.lmm333.weixin.mp.model.HelloWorldModel;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -13,5 +14,13 @@ class HelloWorldController {
     @RequestMapping(value = "hello")
     public HelloWorldModel helloworld() {
         return new HelloWorldModel(1, "myname", 80.88f);
+    }
+
+    @RequestMapping("/hello2")
+    public String hello2(ModelMap map) {
+        // 加入一个属性，用来在模板中读取
+        map.addAttribute("host", "http://lmm333.com");
+        // return模板文件的名称，对应src/main/resources/templates/index.html
+        return "hello";
     }
 }
