07766b499e576148c1ca0ed51cfffa6380073f48
lmmsoft <lmm333@126.com>
2018-08-15 02:30:34 +0800

    Add reveal.js 3.7.0 and qa.html

--- src/main/java/com/lmm333/weixin/mp/controller/ResultController.java
+++ src/main/java/com/lmm333/weixin/mp/controller/ResultController.java
@@ -7,6 +7,7 @@ import com.lmm333.weixin.mp.service.QAService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -54,5 +55,10 @@ public class ResultController {
             //统计页
             return "";
         }
+    }
+
+    @GetMapping("qapage")
+    public String qa() {
+        return "redirect:/qa.html";
     }
 }
